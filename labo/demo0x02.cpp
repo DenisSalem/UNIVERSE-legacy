d487c4c5acced22f744eaf4b8eef9b2e992f2345
DenisSalem <denissalem@tuxfamily.org>
2015-03-21 17:00:48 +0100

    added comments

--- labo/demo0x02.cpp
+++ labo/demo0x02.cpp
@@ -10,13 +10,25 @@
 #include <GLFW/glfw3.h>
 
 void initVAO(GLuint * VertexArrayID) {
+	/* generate vertex array object names */
 	glGenVertexArrays(1, VertexArrayID);
+	/* Bind VAO with VertexArrayID */
 	glBindVertexArray(*VertexArrayID);
 }
 
 void initVBO(GLuint * vertexbuffer, GLfloat * g_vertex_buffer_data) {
+	/* As Above, generate a buffer */
 	glGenBuffers(1, vertexbuffer);
+	/* then bind it to vertexbuffer */
 	glBindBuffer(GL_ARRAY_BUFFER, *vertexbuffer);
+	/* 
+	void glBufferData(GLenum  target,  GLsizeiptr  size,  const GLvoid *  data,  GLenum  usage);
+
+		glBufferData creates a new data store for the buffer object currently bound to
+        	target. Any pre-existing data store is deleted. The new data store is created with the
+        	specified size in bytes and usage. If data
+	        is not NULL, the data store is initialized with data from this pointer. 
+	*/
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*9, g_vertex_buffer_data, GL_STATIC_DRAW);
 }
 
@@ -25,10 +37,22 @@ void compileShader(GLuint shaderID, char * sourcePointer) {
 	glCompileShader(shaderID);
 
 	GLint Result = GL_FALSE;
+	/* prepare buffer for receiving compile log */
 	int InfoLogLength = 1024;
 	char shaderErrorMessage[1024] = {0};
 
+	/* return a parameter from a shader object
+
+	GL_COMPILE_STATUS
+		For implementations that support a shader compiler, 
+		    params returns GL_TRUE if the last compile operation on shader was
+		    successful, and GL_FALSE otherwise.
+	
+		result isn't actually used, but remain for debugging purpose.
+	*/
 	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &Result);
+	
+	/* get the actual log of last compile in shaderErrorMessage array */ 
 	glGetShaderInfoLog(shaderID, InfoLogLength, NULL, shaderErrorMessage);
 	std::cout <<  shaderErrorMessage << "\n" ;
 }
@@ -55,12 +79,6 @@ static void error_callback(int error, const char* description)
 	std::cout << description ;
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
 int main(void)
 {
 	GLFWwindow* window;
@@ -70,10 +88,9 @@ int main(void)
 	if (!glfwInit())
 		exit(-1);
 
-	// use Core OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-	window = glfwCreateWindow(640, 480, "untitled", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Hello White Triangle", NULL, NULL);
 
 	if (!window){
 		glfwTerminate();
@@ -82,12 +99,12 @@ int main(void)
 
 	glfwMakeContextCurrent(window); 
 
-	glfwSetKeyCallback(window, key_callback);
 	if(gl3wInit() != 0) {
 		std::cout << "gl3w fail\n";
 		return 0;
 	};
 	std::cout << glGetString(GL_VERSION) << "\n";
+	
 	
 	GLuint vertexbuffer;
 	GLuint VertexArrayID;
@@ -107,22 +124,30 @@ int main(void)
 	GLchar * fragmentShaderSource =	"#version 330\n"
 					"out vec3 color;\n"
 					"void main() {\n"
-					"	color = vec3(1,0,0);\n"
+					"	color = vec3(1,1,1);\n"
 					"}";
 
+	/* Init VAO and VBO */
 	initVAO(&VertexArrayID);
 	initVBO(&vertexbuffer, g_vertex_buffer_data);
 
+
+	/* Create both fragment and vertex shader */
 	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
 	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
 
+	/* compile them */
 	compileShader(VertexShaderID, vertexShaderSource);
 	compileShader(FragmentShaderID, fragmentShaderSource);
 
+	/* create program */
 	GLuint ProgramID = glCreateProgram();
 
+	/* Bind shaders to program */
     	glAttachShader(ProgramID, VertexShaderID);
     	glAttachShader(ProgramID, FragmentShaderID);
+
+	/* Link it */
     	glLinkProgram(ProgramID);
 
 	GLint Result = GL_FALSE;
@@ -134,13 +159,16 @@ int main(void)
 	glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
 	std::cout << ProgramErrorMessage << "\n";
 
+	/* Installs a program object as part of current rendering state */
 	glUseProgram(ProgramID);
+
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.0f,0.0f,0.0f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		render(vertexbuffer);
+		/* WHOA COOL HOW/WHERE THE FUCK SHADERS ARE LINKED WITH VAO?? */
 
 		glfwSwapBuffers(window);
 	}
