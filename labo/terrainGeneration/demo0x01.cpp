234a26343626237ccdf9676c3a8a0dcdb040dfac
denissalem <denissalem@tuxfamily.org>
2015-09-29 16:11:02 +0200

    supprimé :        "doc/.~lock.Document de r\303\251f\303\251rence.odt#" 	nouveau fichier : labo/terrainGeneration/demo0x00.png 	modifié :         labo/terrainGeneration/demo0x01.cpp 	nouveau fichier : labo/terrainGeneration/demo0x01.png

--- labo/terrainGeneration/demo0x01.cpp
+++ labo/terrainGeneration/demo0x01.cpp
@@ -2,133 +2,110 @@
 #include <cstdlib>
 #include <time.h>
 #include "png.cpp"
-#include <math.h>
 
-unsigned long int getSeed() {
-	timespec tStruct;
+unsigned long int getRandom() {
+        timespec tStruct;
 	clock_gettime(CLOCK_REALTIME, &tStruct);
-	return tStruct.tv_nsec;
+	return tStruct.tv_nsec % 255;
 }
 
-void UNIVERSE_NOISE_1(unsigned long int ** matrix, long int scale, int offsetX, long int offsetY, unsigned long int * max) {
-	long int halfScale= scale >> 1;
-	long int x,y;
-	long int randX0, randY0, randX1, randY1, randX2, randY2,randX3,randY3;
-	randX0 = (getSeed() % halfScale) - halfScale;
-	randY0 = (getSeed() % halfScale) - halfScale;
-	randX1 = (getSeed() % halfScale);
-	randY1 = (getSeed() % halfScale) - halfScale;
-	randX2 = (getSeed() % halfScale);
-	randY2 = (getSeed() % halfScale);
-	randX3 = (getSeed() % halfScale) - halfScale;
-	randY3 = (getSeed() % halfScale);
-	char rand = getSeed() & 15;
-	for(y=0;y<scale;y++) {
-		for(x=0;x<scale;x++) {
-			//TOP RIGHT
-			if((rand & 1 ) && offsetX+x+randX0 >= 0 && offsetX+x+randX0 < scale && y+randY0 >= 0 && y+randY0 < scale) {
-				if (x < y && x < scale-y ) {
-					matrix[offsetX+x+randX0][y+randY0] += x;
-				}
-				else if (x >= scale-y && scale-x <= scale-y) {
-					matrix[offsetX+x+randX0][y+randY0] += scale-x;
-				}
-				else if (y <= x && y < scale-x) {
-					matrix[offsetX+x+randX0][y+randY0] += y;
-				}
-				else if (y >= scale-x && scale-y <= scale-y) {
-					matrix[offsetX+x+randX0][y+randY0] += scale-y;
-				}
-				if (*max < matrix[offsetX+x+randX0][y+randY0]) {
-					*max = matrix[offsetX+x+randX0][y+randY0];
-				}
-			}/*
-			//TOP RIGHT
-			if((rand & 2) && offsetX+x+randX1 >= 0 && offsetX+x+randX1 < scale && y+randY1 >= 0 && y+randY1 < scale) {
-				if (x < y && x < scale-y ) {
-					matrix[offsetX+x+randX1][y+randY1] += x;
-				}
-				else if (x >= scale-y && scale-x <= scale-y) {
-					matrix[offsetX+x+randX1][y+randY1] += scale-x;
-				}
-				else if (y <= x && y < scale-x) {
-					matrix[offsetX+x+randX1][y+randY1] += y;
-				}
-				else if (y >= scale-x && scale-y <= scale-y) {
-					matrix[offsetX+x+randX1][y+randY1] += scale-y;
-				}
-				if (*max < matrix[x+randX1][y+randY1]) {
-					*max = matrix[offsetX+x+randX1][y+randY1];
-				}
+void UNIVERSE_NOISE_1(unsigned long int ** matrix, unsigned long int scale) {
+	unsigned long int x,y; // x,y :p
+	int j=1; // right shit on scale (divide by two)
+	unsigned long int i; (getRandom() % (scale >> j)); // gradient with phase
+	unsigned long int peak; //gradient max
+	char increase;
+	while (scale >> j >= 1) {
+		peak = (scale >> j) + (getRandom() % (scale >> j));
+		increase=1;
+		i= (getRandom() % (scale >> j));
+		for(y=0;y<scale;y++) {
+			if(peak == i) {
+				increase = 0;
 			}
-			//BOTTOM RIGHT
-			if((rand & 4) && offsetX+x+randX2 >= 0 && offsetX+x+randX2 < scale && y+randY2 >= 0 && y+randY2 < scale) {
-				if (x < y && x < scale-y ) {
-					matrix[offsetX+x+randX2][y+randY2] += x;
-				}
-				else if (x >= scale-y && scale-x <= scale-y) {
-					matrix[offsetX+x+randX2][y+randY2] += scale-x;
-				}
-				else if (y <= x && y < scale-x) {
-					matrix[offsetX+x+randX2][y+randY2] += y;
-				}
-				else if (y >= scale-x && scale-y <= scale-y) {
-					matrix[offsetX+x+randX2][y+randY2] += scale-y;
-				}
-				if (*max < matrix[x+randX2][y+randY2]) {
-					*max = matrix[offsetX+x+randX2][y+randY2];
-				}
+			if (y==0 || increase == 0 && i == 0) {
+				peak = (scale >> j) + (getRandom() % (scale >> j));
+				increase = 1;
 			}
-			//BOTTOM LEFT
-			if((rand & 8) && offsetX+x+randX3 >= 0 && offsetX+x+randX3 < scale && y+randY3 >= 0 && y+randY3 < scale) {
-				if (x < y && x < scale-y ) {
-					matrix[offsetX+x+randX3][y+randY3] += x;
-				}
-				else if (x >= scale-y && scale-x <= scale-y) {
-					matrix[offsetX+x+randX3][y+randY3] += scale-x;
-				}
-				else if (y <= x && y < scale-x) {
-					matrix[offsetX+x+randX3][y+randY3] += y;
-				}
-				else if (y >= scale-x && scale-y <= scale-y) {
-					matrix[offsetX+x+randX3][y+randY3] += scale-y;
-				}
-				if (*max < matrix[x+randX3][y+randY3]) {
-					*max = matrix[offsetX+x+randX3][y+randY3];
-				}
-			}*/
+			for(x=0;x<scale;x++) {
+				matrix[x][y] += i;
+			}
+			if (increase == 1) {
+				i++;
+			}
+			else {
+				i--;
+			}
 		}
+		j++;
+	}
+	j = 1;
+	while (scale >> j >= 1) {
+		peak = (scale >> j) + (getRandom() % (scale >> j));
+		increase=1;
+		i= (getRandom() % (scale >> j));
+		for(x=0;x<scale;x++) {
+			if(peak == i) {
+				increase = 0;
+			}
+			if (x==0 || increase == 0 && i == 0) {
+				peak = (scale >> j) + (getRandom() % (scale >> j));
+				increase = 1;
+			}
+			for(y=0;y<scale;y++) {
+				matrix[x][y] += i;
+			}
+			if (increase == 1) {
+				i++;
+			}
+			else {
+				i--;
+			}
+		}
+		j++;
 	}
 }
+
 
 int main(int argc, char ** argv) {
+        unsigned long int scale = atoi(argv[1]);
+        unsigned long int ** matrix = (unsigned long int **) malloc(sizeof(unsigned long int *) * scale);
+        PIXEL ** png = (PIXEL **) malloc(sizeof(PIXEL *) * scale);
+        unsigned long int i,j;
+        for (i=0; i<scale;i++) {
+                matrix[i] = (unsigned long int *) malloc(sizeof(unsigned long int) * scale);
+                png[i] = (PIXEL *) malloc(sizeof(PIXEL) * scale);
+                for(j=0;j<scale;j++) {
+                        matrix[i][j] = 0;
 
-	// Initiate shit n stuff
-	long int scale = atoi(argv[1]);
-	unsigned long int ** matrix = (unsigned long int **) malloc(sizeof(unsigned long int *) * scale);
-	PIXEL ** png = (PIXEL **) malloc(sizeof(PIXEL *) * scale);
-	long unsigned int i,j,max=0;
+                }
+        }
+
+	UNIVERSE_NOISE_1(matrix, scale);
+        // Adjust color Levels
+        int max,min = 65536;
+        for (i=0; i<scale;i++) {
+                for(j=0;j<scale;j++) {
+                        if (matrix[i][j] > max) {
+                                max = matrix[i][j];
+                        }
+                        if (matrix[i][j] < min) {
+                                min = matrix[i][j];
+                        }
+                }
+        }
+
 	char color;
-	for (i=0; i<scale;i++) {
-		matrix[i] = (unsigned long int *) malloc(sizeof(unsigned long int) * scale);
-		png[i] = (PIXEL *) malloc(sizeof(PIXEL) * scale);
-		for(j=0;j<scale;j++) {
-			matrix[i][j] = 0;
+        for (i=0; i<scale;i++) {
+                for(j=0;j<scale;j++) {
+			color = ((matrix[i][j] - min) * 255) / (max-min);
+                        png[i][j].Alpha = 0xFF;
+                        png[i][j].Red = color;
+                        png[i][j].Green = color;
+                        png[i][j].Blue = color;
+                }
+        }
+        writePng(png,scale);
 
-		}
-	}
-	// DRAW MAP RECURSIVELY
-	UNIVERSE_NOISE_1(matrix, scale,256,0, &max);
-	// COLOR LEVEL ADJUSTEMENT
-	for (i=0; i<scale;i++) {
-		for(j=0;j<scale;j++) {
-			color		= ((double) (matrix[i][j]) / max) * 255;
-			png[i][j].Alpha	= 0xFF;
-			png[i][j].Red	= color;
-			png[i][j].Green	= color;
-			png[i][j].Blue	= color;
-		}
-	}
-	writePng(png,scale);
 	return 0;
 }
