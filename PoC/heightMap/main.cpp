85143a87ef85f46d6dba82bc163a80c1096c7295
denissalem <denissalem@tuxfamily.org>
2016-02-03 19:40:52 +0100

    modifié :         PoC/heightMap/main.cpp

--- PoC/heightMap/main.cpp
+++ PoC/heightMap/main.cpp
@@ -11,15 +11,15 @@ unsigned long int getRandom() {
 }
 
 
-void UNIVERSE_MASK_1(long double ** matrix, unsigned long int scale) {
+void UNIVERSE_MASK_1(long double ** matrix, long double scale) {
 	unsigned long int x,y;
-	unsigned long int halfScale = scale >> 1;
-	long  double radius;
+	long double halfScale = scale / 2 ;
+	long double radius;
 	for(x=0;x<scale;x++) {
 		for(y=0;y<scale;y++) {
 			radius = sqrtl( ((y-halfScale) * (y-halfScale)) + ((x-halfScale) * (x-halfScale)));
 			if ( radius < halfScale ) {
-				matrix[x][y] = halfScale - radius;
+				matrix[x][y] = (halfScale - radius) / (halfScale);
 			}
 			else {
 				matrix[x][y] = 0;
@@ -28,23 +28,46 @@ void UNIVERSE_MASK_1(long double ** matrix, unsigned long int scale) {
 	}
 }
 
-void UNIVERSE_NOISE_1(long double * matrix, long double ** mask, unsigned long int scale, unsigned long int offsetX, unsigned long int offsetY, unsigned long int realScale) {
+void UNIVERSE_NOISE_1(long double * matrix, long double ** mask, unsigned long int scale, long int offsetX, long int offsetY, unsigned long int realScale) {
 	if (scale == 1) {
 		return;
 	}
-	unsigned long int halfScale = scale >> 1;
-	unsigned long int x, y;
+	long int halfScale = scale >> 1;
+	long int x, y;
 	long int randX = - halfScale + getRandom() % scale;
 	long int randY = - halfScale + getRandom() % scale;
 	int inc = realScale / scale;
 	unsigned long int maskX=0, maskY=0;
+	long int wrapX,wrapY;
 	char doIt = getRandom() & 1;
 	if (doIt) {
 	for(x=0;x<scale;x++) {
 		maskY = 0;
 			for(y=0;y<scale;y++) {
+				// Easy Case
 				if ( randX + offsetX + x < realScale && randX + offsetX +x >= 0 && randY + offsetY + y < realScale && randY + offsetY + y >= 0) {
 					matrix[ ((x+randX+offsetX) * realScale) + y+randY+offsetY] += mask[maskX][maskY] / (inc);
+				}
+				// Wrap shit n stuff
+				else {
+					wrapX = 0;
+					wrapY = 0;
+
+					if ( randX + offsetX + x >= realScale && randX + offsetX + x < realScale*2 ) {
+						wrapX = - realScale;
+					}
+					else if ( randX + offsetX + x > -realScale && randX + offsetX + x < 0) {
+						wrapX = realScale;
+					}
+
+					if ( randY + offsetY + y > -realScale && randY + offsetY + y < 0) {
+						wrapY = realScale;
+					}
+					else if ( randY + offsetY + y < realScale * 2 && randY + offsetY + y >= realScale) {
+						wrapY = -realScale;
+					}
+
+					matrix[ (  wrapX + x + offsetX  + randX) * realScale + (y+randY+offsetY) + wrapY] += mask[maskX][maskY] / (inc);
 				}
 				maskY+=inc;
 			}
@@ -68,21 +91,21 @@ int main(int argc, char ** argv) {
 	unsigned long int x,y,i,j;
 	long double * matrix;
 	long double ** mask;
-	matrix	= (long double *) malloc( sizeof( long double ) * scale * scale);
+	matrix	= (long double *) malloc( sizeof( long double ) * scale * scale * 12);
 	mask 	= (long double **) malloc( sizeof( long double *) * scale );
-        PIXEL ** png = (PIXEL **) malloc(sizeof(PIXEL *) * scale);
+        PIXEL ** png = (PIXEL **) malloc(sizeof(PIXEL *) * scale * 3);
 
 	for (x=0; x<scale;x++) {
 		mask[x] = (long double *) malloc( sizeof(long  double) * scale );
 	}
 	for (x=0; x<scale;x++) {
 		for(y=0;y<scale;y++){
-			matrix[x*scale+y] = 0;
+			matrix[x*scale*3+y] = 0;
 		}
 		png[x] = (PIXEL *) malloc(sizeof(PIXEL) * scale);
 	}
 
-	UNIVERSE_MASK_1(mask, scale);
+	UNIVERSE_MASK_1(mask, (long double ) scale);
 	UNIVERSE_NOISE_1(matrix, mask, scale, 0, 0, scale);
         // Adjust color Levels
         long double max=0,min = 65536;
@@ -96,6 +119,8 @@ int main(int argc, char ** argv) {
                         }
                 }
         }
+
+	std::cout << max << "\n";
 
         char color;
         for (x=0; x<scale;x++) {
