982cc54736e8d274b5a59da1b5daa3f0c79ce764
denissalem <denissalem@tuxfamily.org>
2016-04-20 17:32:09 +0200

    modifié :         PoC/proceduralPlanet0x00/Makefile 	nouveau fichier : PoC/proceduralPlanet0x00/gl3wInstaller.sh 	nouveau fichier : PoC/proceduralPlanet0x00/proceduralPlanet 	modifié :         PoC/proceduralSphere/Makefile 	supprimé :        PoC/proceduralSphere/gl3w.c 	nouveau fichier : PoC/proceduralSphere/gl3wInstaller.sh 	modifié :         PoC/quickOpenGLdemo/Makefile 	supprimé :        PoC/quickOpenGLdemo/gl3w.c 	nouveau fichier : PoC/quickOpenGLdemo/gl3wInstaller.sh

---
+++ PoC/quickOpenGLdemo/gl3wInstaller.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo "Installing gl3w..."
+if type "git" &> /dev/null; then
+  git clone "http://github.com/skaslev/gl3w"
+  cd gl3w/
+  ./gl3w_gen.py
+  if find "/usr/include/GL/gl3w.h" "/usr/include/GL/glcorearb.h" &> /dev/null; then
+    echo "gl3w is already installed :)"
+  else
+    cp -v include/GL/* /usr/include/GL
+  fi
+  cp -v src/gl3w.c ..
+else
+  echo "git not found..."
+  echo "Aborting..."
+fi
+
