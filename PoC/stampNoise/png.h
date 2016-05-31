feae32c16df527bacde36c66d5323ab1c0d694ae
Nicolas54 <nicolas.deliere@gmail.com>
2016-05-31 15:17:26 +0200

    modified:   PoC/stampNoise/main.faster.cpp 	modified:   PoC/stampNoise/png.cpp 	new file:   PoC/stampNoise/png.h

---
+++ PoC/stampNoise/png.h
@@ -0,0 +1,9 @@
+typedef struct _PIXEL
+{
+     unsigned char Red;
+     unsigned char Green;
+     unsigned char Blue;
+     unsigned char Alpha;
+} PIXEL;
+
+int writePng( PIXEL ** matrix, int size);
